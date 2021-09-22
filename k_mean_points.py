670dab55dda61ac779fb3be6a52c58a79fe389e5
aeksei <aeksei94@gmail.com>
2021-09-22 22:30:11 +0300

    fix apply

--- k_mean_points.py
+++ k_mean_points.py
@@ -1,7 +1,5 @@
 import geopandas as gpd
-from pandarallel import pandarallel
 
-pandarallel.initialize(progress_bar=True)
 
 if __name__ == "__main__":
     DRIVE_FILE_ID = "1kdqgi94qhdEVvOJAvlpHvurYcurh_5d9"
@@ -14,12 +12,11 @@ if __name__ == "__main__":
     url = f"https://drive.google.com/uc?export=download&id={DRIVE_FILE_ID}"
 
     df_roads = gpd.read_file(url).iloc[:100]
+    df_roads = df_roads.set_index("id")
     print(df_roads.head())
 
     K_MEAN_POINTS = 5
-    mean_distance = df_roads.geometry.parallel_apply(
+    mean_distance = df_roads.geometry.apply(
         lambda road: df_kgiop_objects.distance(road).nsmallest(K_MEAN_POINTS).mean())
-
-    mean_distance.hist()
 
     mean_distance.to_json("k_mean_points_distance.json")
