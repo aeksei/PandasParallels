2c06eb2cc9b128d7e2b955198ed5cd8ea41ae551
aeksei <aeksei94@gmail.com>
2021-09-22 22:43:36 +0300

    remove test sample

--- k_mean_points.py
+++ k_mean_points.py
@@ -5,13 +5,13 @@ if __name__ == "__main__":
     DRIVE_FILE_ID = "1kdqgi94qhdEVvOJAvlpHvurYcurh_5d9"
     url = f"https://drive.google.com/uc?export=download&id={DRIVE_FILE_ID}"
 
-    df_kgiop_objects = gpd.read_file(url).iloc[:100]
+    df_kgiop_objects = gpd.read_file(url)
     print(df_kgiop_objects.head())
 
     DRIVE_FILE_ID = "107DtdB8wUehAFoASn-rtQ1jokVam04FN"
     url = f"https://drive.google.com/uc?export=download&id={DRIVE_FILE_ID}"
 
-    df_roads = gpd.read_file(url).iloc[:100]
+    df_roads = gpd.read_file(url)
     df_roads = df_roads.set_index("id")
     print(df_roads.head())
 
